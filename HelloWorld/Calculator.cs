dad84ddf2acb7cc1275fe4b4f92e92a6da6aeb50
Simon Lund <Lund16@gmail.com>
2023-02-09 21:03:58 +0100

    Implement Calculator

---
+++ HelloWorld/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace HelloWorld;
+
+public static class Calculator
+{
+    /// <summary>
+    /// Adds two numbers.
+    /// </summary>
+    /// <param name="a">Number one</param>
+    /// <param name="b">Number two</param>
+    /// <returns>The added numbers.</returns>
+    public static double Add(double a, double b)
+    {
+        return a + b;
+    }
+
+    public static double Subtract(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Multiply(double a, double b)
+    {
+        return a * b;
+    }
+}
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hej hej o/");
+﻿using HelloWorld;
+
+Console.WriteLine("Add numbers!");
+
+Console.Write("Enter number 1: ");
+var input1 = Console.ReadLine();
+
+Console.Write("Enter number 2: ");
+var input2 = Console.ReadLine();
+
+double.TryParse(input1, out var a);
+double.TryParse(input2, out var b);
+
+var result = Calculator.Add(a, b);
+
+if (result == 42)
+{
+    Console.WriteLine("JACKPOT!!!!!!!");
+}
+else if (result > 100)
+{
+    Console.WriteLine("Big number!");
+}
+else
+{
+    Console.WriteLine($"Result: {result}");
+}
